d4c20a97cbeeff6131ad857a0e6ac9b1ad47b369
wargas <teixeira.wargas@gmail.com>
2025-03-23 17:00:04 -0300

    tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import axios from "axios";
 
 export default async function Home() {
 
+  
+
   const { data: lista } = await axios.get(`https://extratos-api.deltex.com.br/list`)
 
   return (
-    <FormFiles tipos={lista} />
+    <FormFiles tipos={lista} />  
   )
 }
