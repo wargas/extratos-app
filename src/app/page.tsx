309a21644dc6186b75752b178a0be120ac3d3379
wargas <teixeira.wargas@gmail.com>
2025-03-23 12:15:44 -0300

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,167 @@
-import Image from "next/image";
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import axios from "axios";
+import EventEmitter from "eventemitter3";
+import { DownloadIcon, PlayIcon, Trash2Icon } from "lucide-react";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const eventEmitter = new EventEmitter()
+const CHANGE_STATUS_NAME = 'change-status'
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [files, setFiles] = useState<{ file: File, tipo: string, id: string, csv?: string, status?: string }[]>([])
+  const [tipo, setTipo] = useState('')
+
+  const refInputFile = useRef<HTMLInputElement>(null)
+
+
+  function handleClickAdicionar(): void {
+    if (!refInputFile.current) {
+      return;
+    }
+    const list = Array.from(refInputFile.current.files || [])
+      .map(f => ({
+        file: f,
+        tipo: tipo, 
+        status: 'Iniciando',
+        id: Math.random().toString(36).substring(2, 15)
+      }))
+
+    if (list.length == 0) {
+      return;
+    }
+
+    setFiles(prev => [...prev, ...list])
+
+    refInputFile.current.value = ''
+  }
+
+  function handleDelete(item: string): void {
+    setFiles(prev => prev.filter(p => p.id != item))
+  }
+
+  function dispatchChangeStatus(id: string, status: string) {
+    eventEmitter.emit(CHANGE_STATUS_NAME, id, status)
+  }
+
+  const handleProcess = useCallback(async function (id: string) {
+
+    dispatchChangeStatus(id, 'Iniciando')
+
+    const formData = new FormData()
+
+    const file = files.find(f => f.id == id)
+
+    if (!file) {
+      return;
+    }
+
+    formData.append('file', file.file)
+    const { data } = await axios.post<{ csv: string }>(`https://extratos-api.deltex.com.br/direct?template=${tipo}`, formData)
+
+    dispatchChangeStatus(id, 'Concluído')
+
+    setFiles(prev => prev.map(f => {
+      if (f.id == id) {
+        f.status  = 'Concluído'
+        f.csv = data.csv
+      }
+      return f;
+    }))
+
+  }, [files, tipo])
+
+  const handleChangeStatus = useCallback((id: string, status: string) => {
+    setFiles(prev => {
+
+      return prev.map(p => {
+        if (p.id == id) {
+          p.status = status
+        }
+
+        return p
+      })
+    })
+  }, [files, tipo])
+
+  useEffect(() => {
+
+    eventEmitter.on(CHANGE_STATUS_NAME, handleChangeStatus)
+
+    return () => {
+      eventEmitter.off(CHANGE_STATUS_NAME, handleChangeStatus)
+    }
+
+  }, [])
+
+  return (
+    <div className="w-full container mx-auto mt-4">
+
+      <div className="flex gap-4">
+        <Input ref={refInputFile} type="file" multiple />
+        <Select onValueChange={v => setTipo(v)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Tipo de Extrato" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="stone1">Stone 01</SelectItem>
+              <SelectItem value="itau1">Itaú 01</SelectItem>
+              <SelectItem value="bb1">Banco do Brasil 01</SelectItem>
+              <SelectItem value="bb2">Banco do Brasil 02</SelectItem>
+              <SelectItem value="bb3">Banco do Brasil 03</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button disabled={tipo == ''} onClick={handleClickAdicionar}>Adicionar</Button>
+      </div>
+      <div className="mt-4 border-t">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Nome</TableHead>
+              <TableHead>Tamanho</TableHead>
+              <TableHead>Tipo</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {files.map(({ file, tipo, id, csv = null, status = '' }) => (
+              <TableRow key={id}>
+                <TableCell>{file.name}</TableCell>
+                <TableCell>{file.size}</TableCell>
+                <TableCell>{tipo}</TableCell>
+                <TableCell>{status}</TableCell>
+                <TableCell>
+                  <div className="flex justify-end">
+                    {csv && <Button size={'sm'} variant={'ghost'} asChild>
+                      <a target="_blank" href={csv}>
+                        <DownloadIcon />
+                      </a>
+                    </Button>}
+                    <Button onClick={() => handleProcess(id)} size={'sm'} variant={'ghost'}>
+                      <PlayIcon />
+                    </Button>
+                    <Button onClick={() => handleDelete(id)} size={'sm'} variant={'ghost'}>
+                      <Trash2Icon />
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <div className="flex justify-end border-t pt-4">
+          <Button disabled>Processar Todos</Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
