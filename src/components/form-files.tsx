b1ee8f94053945bb6b66941b96d42872e4914028
wargas <teixeira.wargas@gmail.com>
2025-03-25 07:53:47 -0300

    checkbox

--- src/components/form-files.tsx
+++ src/components/form-files.tsx
@@ -6,18 +6,33 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectVa
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import axios from "axios";
 import EventEmitter from "eventemitter3";
-import { DownloadIcon, PlayIcon, Trash2Icon } from "lucide-react";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { filter } from "lodash";
+import { AlertCircleIcon, DownloadIcon, Loader2Icon, PlayIcon, Trash2Icon } from "lucide-react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Checkbox } from "./ui/checkbox";
 
 const eventEmitter = new EventEmitter()
 const CHANGE_STATUS_NAME = 'change-status'
+enum FILE_STATUS {
+    ADDED,
+    PROCESSING,
+    SUCCESS,
+    ERROR
+}
+
+type FileItem = {
+    file: File, tipo: string, id: string, csv?: string, status?: FILE_STATUS, selected: boolean
+}
 
 export function FormFiles({ tipos }: { tipos: { name: string, id: string }[] }) {
-    const [files, setFiles] = useState<{ file: File, tipo: string, id: string, csv?: string, status?: string }[]>([])
+    const [files, setFiles] = useState<FileItem[]>([])
     const [tipo, setTipo] = useState('')
 
     const refInputFile = useRef<HTMLInputElement>(null)
 
+    const listSelecteds = useMemo(() => {
+        return filter(files, 'selected')
+    }, [files])
 
     function handleClickAdicionar(): void {
         if (!refInputFile.current) {
@@ -27,8 +42,9 @@ export function FormFiles({ tipos }: { tipos: { name: string, id: string }[] }) 
             .map(f => ({
                 file: f,
                 tipo: tipo,
-                status: 'Iniciando',
-                id: Math.random().toString(36).substring(2, 15)
+                status: FILE_STATUS.ADDED,
+                id: Math.random().toString(36).substring(2, 15),
+                selected: false
             }))
 
         if (list.length == 0) {
@@ -44,38 +60,45 @@ export function FormFiles({ tipos }: { tipos: { name: string, id: string }[] }) 
         setFiles(prev => prev.filter(p => p.id != item))
     }
 
-    function dispatchChangeStatus(id: string, status: string) {
+    function dispatchChangeStatus(id: string, status: FILE_STATUS) {
+
         eventEmitter.emit(CHANGE_STATUS_NAME, id, status)
     }
 
     const handleProcess = useCallback(async function (id: string) {
 
-        dispatchChangeStatus(id, 'Iniciando')
+        try {
+            dispatchChangeStatus(id, FILE_STATUS.PROCESSING)
 
-        const formData = new FormData()
+            const formData = new FormData()
 
-        const file = files.find(f => f.id == id)
+            const file = files.find(f => f.id == id)
 
-        if (!file) {
-            return;
-        }
-
-        formData.append('file', file.file)
-        const { data } = await axios.post<{ csv: string }>(`https://extratos-api.deltex.com.br/direct?template=${tipo}`, formData)
-
-        dispatchChangeStatus(id, 'Concluído')
-
-        setFiles(prev => prev.map(f => {
-            if (f.id == id) {
-                f.status = 'Concluído'
-                f.csv = data.csv
+            if (!file) {
+                return;
             }
-            return f;
-        }))
+
+            formData.append('file', file.file)
+            const { data } = await axios.post<{ csv: string }>(`https://extratos-api.deltex.com.br/direct?template=${tipo}`, formData)
+
+            dispatchChangeStatus(id, FILE_STATUS.SUCCESS)
+
+            setFiles(prev => prev.map(f => {
+                if (f.id == id) {
+                    f.status = FILE_STATUS.SUCCESS
+                    f.csv = data.csv
+                }
+                return f;
+            }))
+        } catch (error) {
+
+            dispatchChangeStatus(id, FILE_STATUS.ERROR)
+        }
 
     }, [files, tipo])
 
-    const handleChangeStatus = useCallback((id: string, status: string) => {
+    const handleChangeStatus = useCallback((id: string, status: FILE_STATUS) => {
+        console.log(id, status)
         setFiles(prev => {
 
             return prev.map(p => {
@@ -88,6 +111,26 @@ export function FormFiles({ tipos }: { tipos: { name: string, id: string }[] }) 
         })
     }, [files, tipo])
 
+    
+
+    function handleChangeSelected(id: string, v: string | boolean): void {
+
+        if(id == '') {
+            setFiles(prev => prev.map(f => ({...f, selected: v as boolean})))
+        }
+        
+        if(id.length > 0) {
+            setFiles(prev => {
+                return prev.map(f => {
+                    if(id == f.id) {
+                        f.selected = v as boolean
+                    }
+                    return f
+                })
+            })
+        }
+    }
+
     useEffect(() => {
 
         eventEmitter.on(CHANGE_STATUS_NAME, handleChangeStatus)
@@ -96,7 +139,7 @@ export function FormFiles({ tipos }: { tipos: { name: string, id: string }[] }) 
             eventEmitter.off(CHANGE_STATUS_NAME, handleChangeStatus)
         }
 
-    }, [])
+    }, [files])
 
     return (
         <div className="w-full container mx-auto mt-4">
@@ -128,6 +171,9 @@ export function FormFiles({ tipos }: { tipos: { name: string, id: string }[] }) 
                     <Table>
                         <TableHeader>
                             <TableRow>
+                                <TableHead>
+                                    <Checkbox onCheckedChange={v => handleChangeSelected('', v)} />
+                                </TableHead>
                                 <TableHead>Nome</TableHead>
                                 <TableHead>Tamanho</TableHead>
                                 <TableHead>Tipo</TableHead>
@@ -136,17 +182,30 @@ export function FormFiles({ tipos }: { tipos: { name: string, id: string }[] }) 
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-                            {files.map(({ file, tipo, id, csv = null, status = '' }) => (
+                            {files.map(({ file, tipo, id, csv = null, status = '', selected }) => (
                                 <TableRow key={id}>
+                                    <TableHead>
+                                    <Checkbox checked={selected} onCheckedChange={(v) => handleChangeSelected(id, v)} />
+                                    </TableHead>
                                     <TableCell>{file.name}</TableCell>
                                     <TableCell>{file.size}</TableCell>
                                     <TableCell>{tipo}</TableCell>
                                     <TableCell>{status}</TableCell>
                                     <TableCell>
-                                        <div className="flex justify-end">
-                                            {csv && <Button size={'sm'} variant={'ghost'} asChild>
+                                        <div className="flex items-center justify-end">
+                                            {status == FILE_STATUS.PROCESSING && (
+                                                <Button size={'sm'} variant={'ghost'}>
+                                                    <Loader2Icon className="animate-spin" />
+                                                </Button>
+                                            )}
+                                            {status == FILE_STATUS.ERROR && (
+                                                <Button size={'sm'} variant={'ghost'}>
+                                                    <AlertCircleIcon color="red" size={10} />
+                                                </Button>
+                                            )}
+                                            {status == FILE_STATUS.SUCCESS && csv && <Button size={'sm'} variant={'ghost'} asChild>
                                                 <a target="_blank" href={csv}>
-                                                    <DownloadIcon />
+                                                    <DownloadIcon color="green" />
                                                 </a>
                                             </Button>}
                                             <Button onClick={() => handleProcess(id)} size={'sm'} variant={'ghost'}>
@@ -162,7 +221,7 @@ export function FormFiles({ tipos }: { tipos: { name: string, id: string }[] }) 
                         </TableBody>
                     </Table>
                     <div className="flex justify-end border-t pt-4">
-                        <Button disabled>Processar Todos</Button>
+                        <Button disabled={listSelecteds.length == 0}>Processar Todos ({listSelecteds.length})</Button>
                     </div>
                 </div>
             )}
