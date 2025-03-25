ef76fb113a650dfc0f3792ebde3640dfdf662ba5
wargas <teixeira.wargas@gmail.com>
2025-03-25 05:11:13 -0300

    cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { FormFiles } from "@/components/form-files";
 import axios from "axios";
-
+export const dynamic = 'force-dynamic'
 export default async function Home() {
-
-  
 
   const { data: lista } = await axios.get(`https://extratos-api.deltex.com.br/list`)
 
